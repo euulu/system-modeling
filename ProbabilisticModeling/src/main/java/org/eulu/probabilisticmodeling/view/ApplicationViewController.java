0ab4c723c87d685f4e0f9f891dc95f4191f8b465
euulu <anegamega@gmail.com>
2024-01-24 10:16:10 +0200

    Lab2: remove unneded imports

--- ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/view/ApplicationViewController.java
+++ ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/view/ApplicationViewController.java
@@ -2,10 +2,7 @@ package org.eulu.probabilisticmodeling.view;
 
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
-import javafx.util.StringConverter;
-import javafx.util.converter.NumberStringConverter;
 
 public class ApplicationViewController {
     @FXML
