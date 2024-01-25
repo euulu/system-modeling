104edc05d83c5b0ddd3b6c55f93befdf58965fc7
euulu <anegamega@gmail.com>
2024-01-25 11:30:43 +0200

    Lab2: change generation upper bound to be exclusive

--- ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/model/NumbersGeneratorModel.java
+++ ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/model/NumbersGeneratorModel.java
@@ -7,7 +7,7 @@ public class NumbersGeneratorModel implements NumbersGenerator {
     public int[] generateNumbers(int upperBound, int groupCount, int generationCount) {
         int[] generatedNumbers = new int[generationCount];
         for (int i = 0; i < generationCount; i++) {
-            int randomNumber = ThreadLocalRandom.current().nextInt(0, upperBound + 1);
+            int randomNumber = ThreadLocalRandom.current().nextInt(0, upperBound);
             generatedNumbers[i] = randomNumber;
         }
 
