1f4a927a659235bcd665191833aefba525efa931
euulu <anegamega@gmail.com>
2024-02-05 18:23:47 +0200

    Lab2: change chart legend item size and icon color

--- ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/view/ApplicationViewController.java
+++ ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/view/ApplicationViewController.java
@@ -79,14 +79,14 @@ public class ApplicationViewController {
 
         public LegendCellFactory(MFXListView<String> listView, String data) {
             super(listView, data);
-            squareIcon = new MFXFontIcon("fas-square", 12);
-            squareIcon.setColor(Color.AQUA);
+            squareIcon = new MFXFontIcon("fas-square", 12, Color.web("8c57ff"));
             render(data);
         }
 
         @Override
         protected void render(String data) {
             super.render(data);
+            setPrefHeight(24.0);
             if (squareIcon != null) getChildren().addFirst(squareIcon);
         }
     }
