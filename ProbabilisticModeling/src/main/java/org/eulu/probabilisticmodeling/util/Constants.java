e77bdc746b5ca230b5b53eefaebb1ebea84c4451
euulu <anegamega@gmail.com>
2024-02-06 13:58:47 +0200

    Lab2: add constants class

---
+++ ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/util/Constants.java
@@ -0,0 +1,7 @@
+package org.eulu.probabilisticmodeling.util;
+
+public class Constants {
+    public static final String EXPORT_ERROR_TITLE = "Помилка експорту даних";
+    public static final String EXPORT_ERROR_MESSAGE = "Експорт даних неможливий. Для здійснення експорту введіть необхідні параметри(Верхню межу генерації, Кількість проміжків, Кількість випробувань) та згенеруйте дані.";
+    public static final String EXPORT_ERROR_BTN_OK = "Зрозуміло";
+}
