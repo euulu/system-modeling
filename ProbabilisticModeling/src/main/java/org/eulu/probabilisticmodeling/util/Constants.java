44ff1dd000c9c708fe52a2b63987e545cdd06ec9
euulu <anegamega@gmail.com>
2024-02-06 15:04:41 +0200

    Lab2: add file error export const messages

--- ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/util/Constants.java
+++ ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/util/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String EXPORT_ERROR_TITLE = "Помилка експорту даних";
     public static final String EXPORT_ERROR_MESSAGE = "Експорт даних неможливий. Для здійснення експорту введіть необхідні параметри(Верхню межу генерації, Кількість проміжків, Кількість випробувань) та згенеруйте дані.";
     public static final String EXPORT_ERROR_BTN_OK = "Зрозуміло";
+    public static final String EXPORT_FILE_ERROR_TITLE = "Помилка експорту даних";
+    public static final String EXPORT_FILE_ERROR_MESSAGE = "Щось пішло не по плану. Спробуй ще раз або перезавантаж програму.";
 }
