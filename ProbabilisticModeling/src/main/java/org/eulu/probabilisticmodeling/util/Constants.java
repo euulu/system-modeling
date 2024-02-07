6bc4319a37f3bcb094457477f2721a85114af1ea
euulu <anegamega@gmail.com>
2024-02-07 14:36:20 +0200

    Lab2: add strings for import data from excel error messages

--- ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/util/Constants.java
+++ ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/util/Constants.java
@@ -6,4 +6,7 @@ public class Constants {
     public static final String EXPORT_ERROR_BTN_OK = "Зрозуміло";
     public static final String EXPORT_FILE_ERROR_TITLE = "Помилка експорту даних";
     public static final String EXPORT_FILE_ERROR_MESSAGE = "Щось пішло не по плану. Спробуй ще раз або перезавантаж програму.";
+    public static final String IMPORT_FILE_ERROR_TITLE = "Помилка імпорту даних";
+    public static final String IMPORT_FILE_NOT_FOUND_ERROR_MESSAGE = "Файл не знайденою";
+    public static final String IMPORT_FILE_PARSE_ERROR_MESSAGE = "Файл не знайденою";
 }
