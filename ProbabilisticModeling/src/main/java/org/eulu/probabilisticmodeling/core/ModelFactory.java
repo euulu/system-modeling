88fb84a2f0ea69a02c5693067dcf9fc0c4a2ffa2
euulu <anegamega@gmail.com>
2024-02-07 11:36:40 +0200

    Lab2: add initialization method for DataIOManager to ModelFactory

--- ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/core/ModelFactory.java
+++ ProbabilisticModeling/src/main/java/org/eulu/probabilisticmodeling/core/ModelFactory.java
@@ -1,15 +1,25 @@
 package org.eulu.probabilisticmodeling.core;
 
+import org.eulu.probabilisticmodeling.model.DataIOManager;
+import org.eulu.probabilisticmodeling.model.DataIOManagerModel;
 import org.eulu.probabilisticmodeling.model.NumbersGenerator;
 import org.eulu.probabilisticmodeling.model.NumbersGeneratorModel;
 
 public class ModelFactory {
     private NumbersGenerator numbersGeneratorModel;
+    private DataIOManager dataIOManagerModel;
 
     public NumbersGenerator getNumbersGeneratorModel() {
         if (numbersGeneratorModel == null) {
             numbersGeneratorModel = new NumbersGeneratorModel();
         }
         return numbersGeneratorModel;
+    }
+
+    public DataIOManager getDataIOManagerModel() {
+        if (dataIOManagerModel == null) {
+            dataIOManagerModel = new DataIOManagerModel();
+        }
+        return dataIOManagerModel;
     }
 }
