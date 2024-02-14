3910f18e0889521105332642879f4cb7e568ef03
euulu <anegamega@gmail.com>
2024-02-14 14:57:40 +0200

    Lab3: add MaterialFX library

--- PredatorPray/src/main/java/org/eulu/predatorpray/PredatorPrayApplication.java
+++ PredatorPray/src/main/java/org/eulu/predatorpray/PredatorPrayApplication.java
@@ -1,5 +1,8 @@
 package org.eulu.predatorpray;
 
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.eulu.predatorpray.core.ModelFactory;
@@ -9,6 +12,14 @@ import org.eulu.predatorpray.core.ViewModelFactory;
 public class PredatorPrayApplication extends Application {
     @Override
     public void start(Stage stage) {
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.MODENA) // Optional if you don't need JavaFX's default theme, still recommended though
+                .themes(MaterialFXStylesheets.forAssemble(true)) // Adds the MaterialFX's default theme. The boolean argument is to include legacy controls
+                .setDeploy(true) // Whether to deploy each theme's assets on a temporary dir on the disk
+                .setResolveAssets(true) // Whether to try resolving @import statements and resources urls
+                .build() // Assembles all the added themes into a single CSSFragment (very powerful class check its documentation)
+                .setGlobal(); // Finally, sets the produced stylesheet as the global User-Agent stylesheet
+
         ModelFactory modelFactory = new ModelFactory();
         ViewModelFactory viewModelFactory = new ViewModelFactory(modelFactory);
         ViewHandler viewHandler = new ViewHandler(stage, viewModelFactory);
