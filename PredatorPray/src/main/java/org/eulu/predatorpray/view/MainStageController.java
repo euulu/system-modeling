561adae632ca3e22d277447f4537006e78f33d23
euulu <anegamega@gmail.com>
2024-02-15 13:24:28 +0200

    Lab3: add fxml fields to the controller

--- PredatorPray/src/main/java/org/eulu/predatorpray/view/MainStageController.java
+++ PredatorPray/src/main/java/org/eulu/predatorpray/view/MainStageController.java
@@ -1,7 +1,32 @@
 package org.eulu.predatorpray.view;
 
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class MainStageController {
+    @FXML
+    public GridPane gpWrapper;
+    @FXML
+    public MFXTextField tfXSize;
+    @FXML
+    public MFXTextField tfYSize;
+    @FXML
+    public MFXTextField tfPreyCount;
+    @FXML
+    public MFXTextField tfPreyReproductionAge;
+    @FXML
+    public MFXTextField tfPreyReproductionPeriod;
+    @FXML
+    public MFXTextField tfPredatorCount;
+    @FXML
+    public MFXTextField tfPredatorReproductionAge;
+    @FXML
+    public MFXTextField tfPredatorReproductionPeriod;
+    @FXML
+    public MFXTextField tfPredatorNoFoodPeriod;
+
     public void init(MainStageViewModel mainStageViewModel) {
-        System.out.println("MainStageController::init");
+        gpWrapper.requestFocus();
     }
 }
