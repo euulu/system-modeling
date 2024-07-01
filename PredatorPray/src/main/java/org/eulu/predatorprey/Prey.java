d8ecff7fb372066f1f9b4ae56762ba701aaeb27c
euulu <anegamega@gmail.com>
2024-07-01 22:10:02 +0300

    Add Prey class

---
+++ PredatorPray/src/main/java/org/eulu/predatorprey/Prey.java
@@ -0,0 +1,12 @@
+package org.eulu.predatorprey;
+
+import javafx.scene.paint.Color;
+
+public class Prey extends Animal {
+//    007add
+//    60b9ff
+    public Prey(int x, int y, int reproductionAge) {
+        super(x, y, reproductionAge);
+        this.setColor(Color.web("#007add"));
+    }
+}
