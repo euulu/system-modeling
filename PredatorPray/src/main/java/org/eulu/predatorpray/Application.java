2dcec9dc43521ea5161d9fd92f4de18e0aeb9140
euulu <anegamega@gmail.com>
2024-07-01 15:38:59 +0300

    Add simple canvas board with test colored cells

--- PredatorPray/src/main/java/org/eulu/predatorpray/Application.java
+++ PredatorPray/src/main/java/org/eulu/predatorpray/Application.java
@@ -3,7 +3,11 @@ package org.eulu.predatorpray;
 import io.github.palexdev.materialfx.theming.JavaFXThemes;
 import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
 import io.github.palexdev.materialfx.theming.UserAgentBuilder;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Application extends javafx.application.Application {
     @Override
@@ -15,6 +19,16 @@ public class Application extends javafx.application.Application {
                 .setResolveAssets(true) // Whether to try resolving @import statements and resources urls
                 .build() // Assembles all the added themes into a single CSSFragment (very powerful class check its documentation)
                 .setGlobal(); // Finally, sets the produced stylesheet as the global User-Agent stylesheet
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-stage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Імітаційне моделювання(Луцюк Є.В. - ЛР№3)");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
--- PredatorPray/src/main/java/org/eulu/predatorpray/Controller.java
+++ PredatorPray/src/main/java/org/eulu/predatorpray/Controller.java
@@ -1,9 +1,98 @@
 package org.eulu.predatorpray;
 
-import javafx.event.ActionEvent;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class Controller {
-    public void onBtnStart(ActionEvent actionEvent) {
+    private static final int MIN_SIZE = 572;
 
+    @FXML
+    public StackPane spEcosystem;
+    @FXML
+    public MFXTextField tfXSize;
+    @FXML
+    public MFXTextField tfYSize;
+    @FXML
+    public MFXTextField tfPreyCount;
+    @FXML
+    public MFXTextField tfPredatorCount;
+
+    public void onBtnStart() {
+        Canvas canvas = new Canvas(MIN_SIZE, MIN_SIZE);
+        spEcosystem.getChildren().add(canvas);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawGrid(canvas, gc);
+    }
+
+    private void drawGrid(Canvas canvas, GraphicsContext gc) {
+        int numColumns = Integer.parseInt(tfXSize.getText());
+        int numRows = Integer.parseInt(tfYSize.getText());
+        int preyCount = Integer.parseInt(tfPreyCount.getText());
+        int predatorCount = Integer.parseInt(tfPredatorCount.getText());
+
+        float cellSize = numColumns > numRows ? (float) MIN_SIZE / numColumns : (float) MIN_SIZE / numRows;
+
+        if (numColumns > numRows) {
+            canvas.setWidth(MIN_SIZE);
+            canvas.setHeight(numRows * cellSize);
+        } else {
+            canvas.setWidth(numColumns * cellSize);
+            canvas.setHeight(MIN_SIZE);
+        }
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColumns; j++) {
+                gc.setFill(Color.LIGHTGRAY);
+                gc.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
+            }
+        }
+
+        gc.setLineWidth(0.5f);
+        gc.setStroke(Color.GRAY);
+        for (int i = 0; i <= numColumns; i++) {
+            gc.strokeLine(i * cellSize, 0, i * cellSize, canvas.getHeight());
+        }
+
+        for (int i = 0; i <= numRows; i++) {
+            gc.strokeLine(0, i * cellSize, canvas.getWidth(), i * cellSize);
+        }
+
+        fillRandomCells(gc, preyCount, Color.BLUE, cellSize);
+        fillRandomCells(gc, predatorCount, Color.RED, cellSize);
+    }
+
+    private void fillRandomCells(GraphicsContext gc, int numCells, Color color, double cellSize) {
+        int numColumns = Integer.parseInt(tfXSize.getText());
+        int numRows = Integer.parseInt(tfYSize.getText());
+        Set<String> occupiedCells = new HashSet<>();
+        Random random = new Random();
+
+        while (numCells > 0) {
+            int col = random.nextInt(numColumns);
+            int row = random.nextInt(numRows);
+            String cellKey = col + "," + row;
+
+            if (!occupiedCells.contains(cellKey)) {
+                colorCell(gc, col, row, color, cellSize);
+                occupiedCells.add(cellKey);
+                numCells--;
+            }
+        }
+    }
+
+    private void colorCell(GraphicsContext gc, int col, int row, Color color, double cellSize) {
+        double x = col * cellSize;
+        double y = row * cellSize;
+
+        gc.setFill(color);
+        gc.fillRect(x, y, cellSize, cellSize);
     }
 }
